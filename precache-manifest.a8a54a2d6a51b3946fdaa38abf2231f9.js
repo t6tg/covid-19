f2b1edb5940021673ddc19ad3fbdcea7071b319d
thanawatgulati <thanawatgulatijames@gmail.com>
2020-03-29 03:28:45 +0700

    Updates

---
+++ precache-manifest.a8a54a2d6a51b3946fdaa38abf2231f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c122a093ec50bba851203869f77bfc79",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "9d40ca5b91dd360f2a01",
+    "url": "/covid-19/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "6b33e07e9892c03bab7f",
+    "url": "/covid-19/static/css/main.43304b7c.chunk.css"
+  },
+  {
+    "revision": "9d40ca5b91dd360f2a01",
+    "url": "/covid-19/static/js/2.33c7f8a7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-19/static/js/2.33c7f8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b33e07e9892c03bab7f",
+    "url": "/covid-19/static/js/main.10221dae.chunk.js"
+  },
+  {
+    "revision": "e84dd164dd3b06d1310b",
+    "url": "/covid-19/static/js/runtime-main.6ca52c11.js"
+  }
+]);
